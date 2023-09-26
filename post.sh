e65d3615d629fd799411f78af9b8077ed51028db
Ayush Jain <ayushmjain@google.com>
2023-09-26 06:23:38 +0000

    change

--- post.sh
+++ post.sh
@@ -2,5 +2,12 @@
 set -o errexit -o nounset -o pipefail
 
 source post.env
-echo "${cloud_run_fe_service_endpoint}"
+
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" ${cloud_run_fe_service_endpoint})
+
+if [ ${STATUS_CODE} != "200" ]; then
+  echo "Three tier web app not ready, received status code: ${STATUS_CODE}"
+  exit 1
+fi
+
 echo "completed post step"
