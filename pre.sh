5dfb7c41d66af706057e23a6960132c19cf69d4f
Ayush Jain <ayushmjain@google.com>
2023-09-26 06:00:10 +0000

    change

--- pre.sh
+++ pre.sh
@@ -3,8 +3,8 @@ set -o errexit -o nounset -o pipefail
 
 IMAGE_TAG=$(gcloud container images list-tags gcr.io/solcat-ayushmjain-dw-49/${1}/three-tier-app-fe --format="value(tags)" --limit=1)
 
-FE_IMAGE="${gcr.io/solcat-ayushmjain-dw-49/${1}/three-tier-app-fe:${IMAGE_TAG}}"
-API_IMAGE="${gcr.io/solcat-ayushmjain-dw-49/${1}/three-tier-app-api:${IMAGE_TAG}}"
+FE_IMAGE="gcr.io/solcat-ayushmjain-dw-49/${1}/three-tier-app-fe:${IMAGE_TAG}"
+API_IMAGE="gcr.io/solcat-ayushmjain-dw-49/${1}/three-tier-app-api:${IMAGE_TAG}"
 
 cat <<EOT >> pre.env
 cloud_run_api_cloud_run_service_image=${API_IMAGE}
